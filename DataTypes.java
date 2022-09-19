ac96ae51a9de220a60263f12e42a3466c71c426f
Aendary Elia <aendary.elia@mrjaffesclass.com>
2022-09-19 11:43:30 -0700

    Awesome

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes 
+{
+    public static void main(String[] args) 
+    {
+        int i;
+        float f = 2.5f;
+        boolean flag = true;
+        i = 10; 
+        f += i;
+        flag = !flag;
+        i++;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
+    }
+}
